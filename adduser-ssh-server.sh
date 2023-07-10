c1a1f3056bd175afe525f7ab50ffcd2b53cd99f4
roozemain <rozemyneroozemain@gmail.com>
2023-07-10 17:22:10 +0330

    feat: update add user

--- adduser-ssh-server.sh
+++ adduser-ssh-server.sh
@@ -1,15 +1,23 @@
+if ! [ -f "$FILE" ]; then
+	tee adduser-batch.sh << EOF
+#! /usr/bin/env bash
+
+EOF
+fi
+
 #! /usr/bin/env bash
 
 if [ $(id -u) -eq 0 ]; then
   read -p "Enter username : " username
   read -s -p "Enter password : " password
-  egrep "^$username" /etc/passwd >/dev/null
+  grep -E "^$username" /etc/passwd >/dev/null
   if [ $? -eq 0 ]; then
     echo "$username exists!"
     exit 1
   else
     pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
     useradd -m -p "$pass" "$username"
+    [ $? -eq 0 ] && echo -e "\nuseradd -m -p "\"$pass\"" "\"$username\""\n" >> adduser-batch.sh
     [ $? -eq 0 ] && printf "\n🖥️ User has been added to system! ✅\n" || printf "\n🖥️ Failed to add a user! ❌\n"
   fi
 else
